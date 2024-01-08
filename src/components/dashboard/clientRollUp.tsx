a45ae8778db9bdbc4cd492aa5fe36e73e5c849c3
lavi-kumar-lab49 <93575858+lavi-kumar-lab49@users.noreply.github.com>
2024-01-08 20:05:34 +0530

    Added rollup data card

---
+++ src/components/dashboard/clientRollUp.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
+
+
+export function ClientRollUpCard() {
+  return <Card className="basis-1/4  bg-opacity-60">
+    <CardBody className="flex flex-col items-center p-3">
+      <Typography variant="h2">
+        5
+      </Typography>
+      <Typography variant="h6">
+        Clients
+      </Typography>
+    </CardBody>
+  </Card>;
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,14 @@
+import { ClientRollUpCard } from "../components/dashboard/clientRollUp";
 
 export default function Dashboard() {
-  return <>Dashboard</>
+  return <div className=" w-full px-4 flex flex-col gap-3 pt-3 ">
+    <div className="flex flex-row  gap-3  ">
+      <ClientRollUpCard></ClientRollUpCard>
+      <ClientRollUpCard></ClientRollUpCard>
+      <ClientRollUpCard></ClientRollUpCard>
+      <ClientRollUpCard></ClientRollUpCard>
+    </div>
+  </div>
 }
 
 
