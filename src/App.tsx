765f3ef6cafff0cfccbd117c5f6e4ff0fbb543c4
lavi-kumar-lab49 <93575858+lavi-kumar-lab49@users.noreply.github.com>
2024-01-08 20:11:51 +0530

    Added Dashboard Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
-    <div className=' flex  w-100 overflow-hidden' >
+    <div className=' flex  w-100 overflow-hidden bg-blue-200 ' >
       <SideBar></SideBar>
       <Outlet/>
     </div>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,7 @@
 import { ClientRollUpCard } from "../components/dashboard/clientRollUp";
+import ClientList from "../components/dashboard/clientList";
+import SectorAllocation from "../components/dashboard/sectorAllocation";
+import Performace from "../components/dashboard/performance";
 
 export default function Dashboard() {
   return <div className=" w-full px-4 flex flex-col gap-3 pt-3 ">
@@ -7,6 +10,13 @@ export default function Dashboard() {
       <ClientRollUpCard></ClientRollUpCard>
       <ClientRollUpCard></ClientRollUpCard>
       <ClientRollUpCard></ClientRollUpCard>
+    </div>
+    <div className="flex flex-row gap-3 " style={{maxHeight:'calc(100vh - 140px)'}}>
+      <div className="basis-2/3 flex flex-col gap-3">
+        <div  style={{height:'50%'}}>Performace Chart</div>
+        <div style={{height:'50%'}}>ClientList</div>
+      </div>
+      <div className="basis-1/3 ">Sector</div>
     </div>
   </div>
 }
