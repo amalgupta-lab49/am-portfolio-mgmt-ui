490a7599f7ed35d4c10fad4dee63a5d79698e275
lavi-kumar-lab49 <93575858+lavi-kumar-lab49@users.noreply.github.com>
2024-01-08 20:04:05 +0530

    Added sidebar menu

--- src/pages/sideBar.tsx
+++ src/pages/sideBar.tsx
@@ -1,4 +1,53 @@
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Button
+} from "@material-tailwind/react";
+import {
+  UserCircleIcon,
+  InboxIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+import {  Link } from "react-router-dom";
 
 export function SideBar() {
- return <>SideBar</>
+ 
+  return (
+    <Card className="h-[calc(100vh)] shadow-2xl bg-opacity-60 rounded-none">
+      <div className="mb-2 p-4">
+        <Typography variant="h5" color="blue-gray">
+          Sidebar
+        </Typography>
+      </div>
+      <List>
+        <ListItem>
+          <ListItemPrefix>
+            <InboxIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          <Link to={`/`}>Dashboard</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <InboxIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          <Link to={`portfolio`}>Portfolio</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          <Link to={`clients`}>Clients</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  );
 }
